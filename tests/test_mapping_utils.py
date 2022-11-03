b092bf4d4561c9d9bfa96f98980cffee850ac4a6
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-11-02 21:48:29 -0500

    test all methods (#4)
    * test all methods
    
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>
    
    * add test for custom encoder
    
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>
    
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- tests/test_mapping_utils.py
+++ tests/test_mapping_utils.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from darbia.utils.mappings import dict_compare
+from darbia.utils.mappings import dict_compare, get_nested_dict_value
 
 
 def test_dict_compare() -> None:
@@ -10,3 +10,11 @@ def test_dict_compare() -> None:
     new_dict = {"b": 3, "c": 4, "d": 7}
     comparison_results = (["c"], ["a"], {"b": (2, 3)}, ["d"])
     assert dict_compare(old_dict=old_dict, new_dict=new_dict) == comparison_results
+
+
+def test_get_nested_dict_value() -> None:
+    data = {"key": {"path": "value"}}
+    result_value = get_nested_dict_value(data, "key.path")
+    assert result_value == "value"
+    result_none = get_nested_dict_value(data, "key.other")
+    assert result_none is None
--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -2,7 +2,16 @@
 
 from __future__ import annotations
 
-from darbia.utils.strings import random_string, split_prefix_and_number
+import json
+from datetime import timedelta
+
+from darbia.utils.strings import (
+    CustomEncoder,
+    bulk_substring_remove,
+    find_nth,
+    random_string,
+    split_prefix_and_number,
+)
 
 
 def test_random_string() -> None:
@@ -11,3 +20,17 @@ def test_random_string() -> None:
 
 def test_split_prefix_and_number() -> None:
     assert split_prefix_and_number("a1") == ("a", 1)
+
+
+def test_custom_encoder() -> None:
+    assert json.dumps(timedelta(seconds=15), cls=CustomEncoder) == '"0:00:15"'
+
+
+def test_find_nth() -> None:
+    assert find_nth("here's johnny", "o", 1) == 8
+    assert find_nth("orale ya vamos", "a", 3) == 10
+
+
+def test_bulk_substring_remove() -> None:
+    assert bulk_substring_remove("here's johnny", ["he", "joh"]) == "re's nny"
+    assert bulk_substring_remove("orale ya vamos", ["ya", "am"]) == "orale  vos"
