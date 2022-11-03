3b806d539375a11a258eb3a5ae503ef041083814
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-11-02 21:26:22 -0500

    test all methods
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -2,7 +2,12 @@
 
 from __future__ import annotations
 
-from darbia.utils.strings import random_string, split_prefix_and_number
+from darbia.utils.strings import (
+    bulk_substring_remove,
+    find_nth,
+    random_string,
+    split_prefix_and_number,
+)
 
 
 def test_random_string() -> None:
@@ -11,3 +16,13 @@ def test_random_string() -> None:
 
 def test_split_prefix_and_number() -> None:
     assert split_prefix_and_number("a1") == ("a", 1)
+
+
+def test_find_nth() -> None:
+    assert find_nth("here's johnny", "o", 1) == 8
+    assert find_nth("orale ya vamos", "a", 3) == 10
+
+
+def test_bulk_substring_remove() -> None:
+    assert bulk_substring_remove("here's johnny", ["he", "joh"]) == "re's nny"
+    assert bulk_substring_remove("orale ya vamos", ["ya", "am"]) == "orale  vos"
