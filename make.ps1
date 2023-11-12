517af410d18a74fdeefd6d4dbaca443bfb02cdea
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-11-12 20:13:54 +0000

    Remove update deps task
    Signed-off-by: GitHub <noreply@github.com>

--- make.ps1
+++ make.ps1
@@ -9,7 +9,6 @@ USAGE
 COMMANDS
     init              install Python build tools
     install-dev       install local package in editable mode
-    update-deps       update the dependencies
     upgrade-deps      upgrade the dependencies
     lint              run `pre-commit` and `ruff`
     test              run `pytest`
@@ -19,7 +18,7 @@ COMMANDS
 #>
 param(
     [Parameter(Position = 0)]
-    [ValidateSet("init", "install-dev", "update-deps", "upgrade-deps", "lint", "test", "build-dist", "clean", "help")]
+    [ValidateSet("init", "install-dev", "upgrade-deps", "lint", "test", "build-dist", "clean", "help")]
     [string]$Command
 )
 
@@ -38,24 +37,10 @@ function Invoke-Install-Dev
     python -m pip install --upgrade --editable ".[dev, tests, docs]"
 }
 
-function Invoke-Update-Deps
-{
-    python -m pip install --upgrade --editable ".[dev, tests, docs]"
-    python -m pip install --upgrade pip-tools
-    pip-compile --output-file requirements/requirements.txt requirements/requirements.in
-    pip-compile --output-file requirements/requirements-dev.txt requirements/requirements-dev.in
-    pip-compile --output-file requirements/requirements-tests.txtrequirements/requirements-tests.in
-    pip-compile --output-file requirements/requirements-docs.txt requirements/requirements-docs.in
-}
-
 function Invoke-Upgrade-Deps
 {
-    python -m pip install --upgrade pip-tools pre-commit
+    python -m pip install --upgrade pre-commit
     pre-commit autoupdate
-    pip-compile --upgrade --output-file requirements/requirements.txt requirements/requirements.in
-    pip-compile --upgrade --output-file requirements/requirements-dev.txt requirements/requirements-dev.in
-    pip-compile --upgrade --output-file requirements/requirements-tests.txtrequirements/requirements-tests.in
-    pip-compile --upgrade --output-file requirements/requirements-docs.txt requirements/requirements-docs.in
 }
 
 function Invoke-Lint
@@ -100,9 +85,6 @@ switch ($Command)
     }
     "lint"  {
         Invoke-Lint
-    }
-    "update-deps"  {
-        Invoke-Update-Deps
     }
     "upgrade-deps"  {
         Invoke-Upgrade-Deps
